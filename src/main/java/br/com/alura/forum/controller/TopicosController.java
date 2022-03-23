13f7cb96ea7bc77ce1a1188b24603af62cf527d4
Lucas Moreira <lucas.moreira@evoluum.com.br>
2022-03-23 14:25:09 -0300

    Projeto criado

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,44 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping()
+    public List<TopicoDto> lista(String nomeCurso) {
+        return TopicoDto.converter(nomeCurso != null && !nomeCurso.isEmpty() ? topicoRepository.findByCursoNome(nomeCurso) : topicoRepository.findAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
+        Topico topico = topicoRepository.save(topicoForm.converter(cursoRepository));
+
+        URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
+}
