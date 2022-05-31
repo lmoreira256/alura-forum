4ecd08a6633a50533be3abb574c4b6f8e7413da5
Lucas Moreira <lucas.moreira@evoluum.com.br>
2022-05-31 16:36:43 -0300

    Ajustado configs para prod

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -4,7 +4,6 @@ import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
